a48528c6b03ead696d54bde21a8ab842ebc14cce
xuantungcb3 <xuantungcb3@gmail.com>
2016-11-21 22:26:24 +0700

    update service project

---
+++ Service/src/main/test/com/fit/RestWS.java
@@ -0,0 +1,21 @@
+package com.fit;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("RestWS")
+public class RestWS {
+
+	@GET
+	@Path("hello/{name}")
+	@Produces(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.TEXT_PLAIN)
+	public String hello(@PathParam("name") String name) {
+		return "Hello " + name;
+	}
+
+}
